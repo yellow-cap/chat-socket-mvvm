aa1ee7db4a77c15e4488884cc4ac65cbdb780706
Artem <artem@wildapricot.com>
2021-02-05 10:40:46 +0300

    add new app

---
+++ chat/chat/chatApp.swift
@@ -0,0 +1,18 @@
+//
+//  chatApp.swift
+//  chat
+//
+//  Created by Artem on 05.02.2021.
+//
+//
+
+import SwiftUI
+
+@main
+struct chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
