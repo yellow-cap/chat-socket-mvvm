9e8f3ecc12d8ddb83060ab3f58063ac8ed598920
Artem <artem@wildapricot.com>
2021-02-05 11:31:38 +0300

    add auth and chat views

--- chat/chat/Presentation/Authentication/AuthenticationView.swift
+++ chat/chat/Presentation/Authentication/AuthenticationView.swift
@@ -1,8 +1,35 @@
 import SwiftUI
 
 struct AuthenticationView: View {
+    @State var name = ""
+    @State var isChatActive: Bool = false
+
     var body: some View {
-        Text("Authentication view")
+        VStack {
+            VStack {
+                TextField("Enter your name", text: self.$name)
+                        .foregroundColor(Color(red: 0, green: 0, blue: 0))
+
+            }
+                    .padding(7)
+                    .background(RoundedRectangle(cornerRadius: 8))
+                    .foregroundColor(Color(red: 239/255, green: 239/255, blue: 240/255))
+
+            Button("Start chat") {
+                if name.isEmpty {
+                    return
+                }
+
+                isChatActive = true
+            }
+
+            NavigationLink(destination: ChatView(), isActive: $isChatActive) {
+                EmptyView()
+            }
+        }
+                .navigationBarTitle("Authentication")
+                .padding(20)
+
     }
 }
 
