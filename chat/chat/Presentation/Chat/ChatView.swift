48d7b0d15ceaa7a7443046b5ee55b8f3c7fdb0dd
Artem <artem@wildapricot.com>
2021-02-06 18:01:43 +0300

    add send functionality

--- chat/chat/Presentation/Chat/ChatView.swift
+++ chat/chat/Presentation/Chat/ChatView.swift
@@ -29,7 +29,7 @@ struct ChatView: View {
                         return
                     }
 
-                    // send
+                    chatViewModel.send(message: message)
                 }
             }
         }
